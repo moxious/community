2c996a439a2b989f9d04ac0b9bc4a8fca0fe0468
moxious <david.allen@grafana.com>
2024-09-29 16:51:04 -0400

    automatic build of media list

--- generate.py
+++ generate.py
@@ -2,23 +2,25 @@ import os
 from datetime import datetime
 import subprocess
 
-def generate_markdown(title, content):
+def generate_markdown(basedir, title, content):
     # Prepare the filename with the required Jekyll format (YYYY-MM-DD-title.md)
     date_str = datetime.now().strftime("%Y-%m-%d")
     slug = title.lower().replace(" ", "-")
     filename = f"{date_str}-{slug}.md"
-    filepath = os.path.join('_posts', filename)
+    filepath = os.path.join(basedir, filename)
+    return generate_markdown_file(filepath, content)
 
+def generate_markdown_file(filepath, content):
     # Write the file with front matter and content
     with open(filepath, 'w') as file:
         file.write(f"---\n")
-        file.write(f"title: \"{title}\"\n")
         file.write(f"date: {datetime.now().isoformat()}\n")
         file.write(f"layout: post\n")
         file.write(f"---\n\n")
         file.write(content)
 
-    print(f"Generated {filename}")
+    print(f"Generated {filepath}")
+    return filepath
 
 def git_commit_and_push(filename, commit_message="Add new post"):
     subprocess.run(['git', 'add', filename])
@@ -26,12 +28,33 @@ def git_commit_and_push(filename, commit_message="Add new post"):
     subprocess.run(['git', 'push'])
 
 # Example usage
-title = "My New Post"
-content = "This is the content of my new post."
-slug = 'first'
-generate_markdown(title, content)
+# title = "My Second Post"
+# content = "This is the content of my second post."
 
 # Example usage
-generate_markdown(title, content)
-date_str = datetime.now().strftime("%Y-%m-%d")
-git_commit_and_push(f'_posts/{date_str}-{slug}.md', "Added a new blog post")
+# path = generate_markdown("_posts", title, content)
+# date_str = datetime.now().strftime("%Y-%m-%d")
+# git_commit_and_push(path, "Added a new blog post")
+
+def get_media():
+    import json
+    with open(os.path.join(os.path.dirname(__file__), 'media.json')) as f:
+        return json.load(f)
+
+def generate_media_markdown():
+    media = get_media()
+    markdown = ""
+    for m in media:
+        print(m)
+        name = m["name"]
+        url = m["url"]
+        summary = m.get("summary", "No summary available")
+        type = m["type"]
+        author = m.get("author", "Unknown")
+        markdown = markdown + f"## {name}\n\n[LINK]({url})\n\nType: {type}, Author: {author}\n\nSummary: {summary}\n\n"
+    
+    path = generate_markdown_file("_media/media-list.md", markdown)
+    return git_commit_and_push(path, "Add new media post")
+
+if __name__ == "__main__":
+    generate_media_markdown()
